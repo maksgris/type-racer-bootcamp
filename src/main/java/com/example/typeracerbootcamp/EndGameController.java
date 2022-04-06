49805cc07e0b17122ffb7663f3ea8c3afa96da92
AgneVasiu <agne.vasiukeviciute@stud.viko.lt>
2022-04-06 11:33:46 +0300

    Game view

---
+++ src/main/java/com/example/typeracerbootcamp/EndGameController.java
@@ -0,0 +1,20 @@
+package com.example.typeracerbootcamp;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class EndGameController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
